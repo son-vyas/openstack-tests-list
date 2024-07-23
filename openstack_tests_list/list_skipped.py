972d24e641de231183c2ee89da4f188349df8564
Soniya29 <svyas@redhat.com>
2024-07-23 21:14:05 +0530

    Fixing black lint issues (#16)
    Depends-On: #17

--- openstack_tests_list/list_skipped.py
+++ openstack_tests_list/list_skipped.py
@@ -1,16 +1,24 @@
 import yaml
 
+
 def load_yaml(file_path):
-    with open(file_path, 'r') as file:
+    """Load a YAML file and return its content."""
+    with open(file_path, "r") as file:
         return yaml.safe_load(file)
 
+
 def get_skipped_tests(file_path):
+    """Parse the skiplist YAML file and return a dictionary of skipped tests."""
     data = load_yaml(file_path)
     skipped_tests = {}
-    for failure in data.get('known_failures', []):
-        test = failure['test']
-        deployment = failure['deployment']
-        releases = failure['releases']
-        jobs = failure['jobs']
-        skipped_tests[test] = {'deployment': deployment, 'releases': releases, 'jobs': jobs}
+    for failure in data.get("known_failures", []):
+        test = failure["test"]
+        deployment = failure["deployment"]
+        releases = failure["releases"]
+        jobs = failure["jobs"]
+        skipped_tests[test] = {
+            "deployment": deployment,
+            "releases": releases,
+            "jobs": jobs,
+        }
     return skipped_tests
