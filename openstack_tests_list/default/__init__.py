42817ba204eedb7a08402455c1d40bcaf8ca016e
Soniya Vyas <svyas@redhat.com>
2024-07-29 13:02:08 +0530

    Add __init__.py
    - Added __init__.py to default and openstack-tests-list directory to mark it as a Python package.
    - Enables proper imports and package initialization.

---
+++ openstack_tests_list/default/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module contains the default allowlist and skiplist configurations
+for the openstack-tests-list tool.
+"""
+
+from openstack_tests_list.list_allowed import get_allowed_tests
+from openstack_tests_list.list_skipped import get_skipped_tests
