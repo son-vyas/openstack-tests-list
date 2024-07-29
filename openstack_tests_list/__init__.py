a96c7175dd0a57b1b6207de68bcdb39faea6a4f0
Soniya29 <svyas@redhat.com>
2024-07-29 16:40:39 +0530

    Add __init__.py (#25)
    - Added __init__.py to default and openstack-tests-list directory to mark it as a Python package.
    - Enables proper imports and package initialization.

---
+++ openstack_tests_list/__init__.py
@@ -0,0 +1,9 @@
+# openstack_tests_list/__init__.py
+
+# Import submodules or specific classes/functions to be accessible at the package level
+from .list_allowed import ListAllowedYaml
+from .list_skipped import ListSkippedYaml
+from .validate import ValidateYaml
+
+# Define package metadata
+__version__ = "0.1.0"
